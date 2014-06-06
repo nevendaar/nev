4a209c85b9e009d2ed2def040eb4f49f337d7029
Ivan <kia84@mail.msiu.ru>
2014-06-06 18:23:10 +0400

    Start forum/_post refactoring.

--- helper.rb
+++ helper.rb
@@ -2,6 +2,15 @@
 
 class Helper
 
+  RANK_DIR = '/Forumdata/Rank/Ranks'.freeze
+  USER_GROUPS = {
+      :master    => 14,
+      :admin     => 4,
+      :moderator => 3,
+      :banned    => 255,
+      :user      => 1
+  }
+
   def initialize(hash = {})
     @params = hash
     @params[:meta]     = {}
@@ -58,6 +67,52 @@ class Helper
 
   def vk_comments_box(uid)
     "<div class=\"VKborders\"><div id=\"vk_comments\" data-uid=\"#{uid}\"></div></div>"
+  end
+
+  def group_is(code, key_or_group, *groups)
+    not_flag = (key_or_group == :not)
+    operator = not_flag ? '!=' : '='
+    l_groups = not_flag ? groups : [key_or_group].push(*groups)
+    result = l_groups.each_with_object([]) do |group, arr|
+      arr << "#{code}#{operator}#{USER_GROUPS[group.to_sym]}"
+    end.join(not_flag ? ' && ' : ' || ')
+    l_groups.size > 1 ? "(#{result})" : result
+  end
+
+  # Вырезаем из строки ранг и проводим к целому.
+  def user_rank
+    # $USER_RANK_ICON$ = '<img alt="" name="rankimg" border="0" src="http://nevendaar.com/Forumdata/Rank/Ranks/rank10.gif" align="absmiddle" />'
+    "<? 0 + substr($USER_RANK_ICON$, #{home_url.size + RANK_DIR.size + 48}, 2) ?>"
+  end
+
+  def first_post
+    "substr($NUMBER$,-6,3)='>1<'"
+  end
+
+  def not_first_post
+    "substr($NUMBER$,-6,3)!='>1<'"
+  end
+
+  def not_new_on_the_site_tread
+    '$TID$!=1647'
+  end
+
+  def not_lucky_pub_tread
+    '$TID$!=699'
+  end
+
+  def role_game_section
+    %w[6 8 9 10 11 12 31].each_with_object([]) do |id, arr|
+      arr << "$FID$=#{id}"
+    end.join(' || ')
+  end
+
+  def moder_names
+    nicks = %w(Вансан Гангрен vertus SoCrat Пророк Вильгельм Sqwall Hierophant FairYng Inno Mystique Химера Город)
+    nicks.each_with_object([]) do |name, arr|
+      str = "#{name}<"
+      arr << "substr($EDITEDBY$,23,#{str.size})='#{str}'"
+    end.join(' || ')
   end
 
   def layout(template = :main)
