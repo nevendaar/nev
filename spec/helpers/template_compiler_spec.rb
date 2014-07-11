5e6aa6259444a91552352e3210ba6e32de686ff1
Ivan <kia84@mail.msiu.ru>
2014-07-11 11:46:34 +0400

    Move templates for test to fixtures directory.

--- spec/helpers/template_compiler_spec.rb
+++ spec/helpers/template_compiler_spec.rb
@@ -2,109 +2,54 @@
 
 require 'spec_helper'
 
-SIMPLE_HTML = <<-HTML.freeze
-<!DOCTYPE html>
-<html>
-    <head><title>title</title></head>
-    <body></body>
-</html>
-HTML
-
-ERB_HTML = <<-ERB.freeze
-<!DOCTYPE html>
-<html>
-    <head><title>title</title></head>
-    <body><%= "Hello, test!" %></body>
-</html>
-ERB
-
-ERB_LAYOUT = <<-ERB.freeze
-<!DOCTYPE html>
-<html>
-    <head><title>title</title></head>
-    <body>
-<% yield if block_given? %>
-    </body>
-</html>
-ERB
-
-def erb_template(layout_path)
-  <<-ERB.freeze
-<!-- content before -->
-<% layout "#{layout_path}" do %>
-    MAIN CONTENT HERE!
-<% end %>
-<!-- content after -->
-  ERB
-end
-
 describe TemplateCompiler do
 
   context 'html files:' do
-    before :each do
-      @tempfile = Tempfile.new(['template', '.html'])
-    end
-
-    after :each do
-      @tempfile.close
-      @tempfile.unlink # deletes the temp file
+    before :all do
+      @path       = 'spec/fixtures/plain.html'
+      @plain_html = File.read(@path)
+      # Always trim multiple "\n"
+      @plain_html.gsub!(/\n\s*\n+/, "\n")
+      @plain_html.freeze
     end
 
     it 'compile html file' do
-      @tempfile.write SIMPLE_HTML
-      @tempfile.close
-      expect(TemplateCompiler.compile(@tempfile.path, true)).to eq(SIMPLE_HTML)
+      expect(TemplateCompiler.compile(@path, true)).to eq(@plain_html)
     end
 
     it 'trim utf-8 BOM' do
-      @tempfile.write "#{TemplateCompiler.const_get(:BOM_TOKEN)}#{SIMPLE_HTML}"
-      @tempfile.close
-      expect(TemplateCompiler.compile(@tempfile.path, true)).to eq(SIMPLE_HTML)
+      token = TemplateCompiler.const_get(:BOM_TOKEN)
+      expect(TemplateCompiler.compile(@path, true).start_with?(token)).to eq(false)
     end
 
     it 'trim whitespaces by default' do
-      trimmed_html = SIMPLE_HTML.dup
-      trimmed_html.gsub!(/\n\s*\n+/, "\n") # Trim multiple "\n"
-      trimmed_html.gsub!(/ {2,}/, ' ')     # Trim whitespaces
-      @tempfile.write "#{SIMPLE_HTML}#{"\n" * 5}"
-      @tempfile.close
-      expect(TemplateCompiler.compile(@tempfile.path)).to eq(trimmed_html)
+      trimmed_html = @plain_html.dup
+      trimmed_html.gsub!(/ {2,}/, ' ') # Trim whitespaces
+      expect(TemplateCompiler.compile(@path)).to eq(trimmed_html)
     end
   end
 
   context 'erb files:' do
-    before :each do
-      @tempfile = Tempfile.new(['template', '.html.erb'])
-    end
-
-    after :each do
-      @tempfile.close
-      @tempfile.unlink # deletes the temp file
-    end
-
     it 'compile erb file' do
-      result_html = ERB_HTML.dup.gsub!('<%= "Hello, test!" %>', 'Hello, test!')
-      @tempfile.write ERB_HTML
-      @tempfile.close
-      expect(TemplateCompiler.compile(@tempfile.path, true)).to eq(result_html)
+      path = 'spec/fixtures/plain.html.erb'
+      result_html = File.read(path).gsub!("<%= 'Hello, test!' %>", 'Hello, test!')
+      expect(TemplateCompiler.compile(path, true)).to eq(result_html)
     end
 
     it 'compile template with layout' do
-      @layout = Tempfile.new(['layout', '.html.erb'])
-      @layout.write ERB_LAYOUT
-      @layout.close
+      layout_path = 'spec/fixtures/layout.html.erb'
+      layout = File.read(layout_path)
 
-      template = erb_template(@layout.path)
-      @tempfile.write template
-      @tempfile.close
+      template_path = 'spec/fixtures/template.html.erb'
+      template_html = File.read(template_path)
 
-      arr = template.split("\n")
-      layout = ERB_LAYOUT.split("<% yield if block_given? %>\n")
+      arr = template_html.split("\n")
+      layout = layout.split("<% yield if block_given? %>\n")
       arr[1] = layout[0].chomp!
       arr[3] = layout[1].chomp!
 
       result_html = arr.join("\n") << "\n"
-      expect(TemplateCompiler.compile(@tempfile.path, true)).to eq(result_html)
+      expect(TemplateCompiler.compile(template_path, true)).to eq(result_html)
     end
   end
 end
