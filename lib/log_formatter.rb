78d766d1b0b1c382b3be618afa42eca842763832
kia84 <kia84@mail.msiu.ru>
2014-08-24 16:16:55 +0400

    LogFormatter::FORMAT is a constant.

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -3,7 +3,7 @@
 require 'logger'
 
 class LogFormatter < Logger::Formatter
-  Format = "%s, [%s#%d] %s -- %s: %s\n"
+  FORMAT = "%s, [%s#%d] %s -- %s: %s\n"
 
   SEVERITY_TO_COLOR_MAP = {
       'DEBUG' => '0;37', 'INFO'  => '32',   'WARN' => '33',
@@ -18,7 +18,7 @@ class LogFormatter < Logger::Formatter
     color = SEVERITY_TO_COLOR_MAP[severity]
     formatted_severity = "\033[#{color}m#{'%5s' % severity.downcase}\033[0m"
     formatted_progname = progname ? "\033[0;36m#{progname}\033[0m" : progname
-    Format % [severity[0..0], format_datetime(time), $$, formatted_severity,
+    FORMAT % [severity[0..0], format_datetime(time), $$, formatted_severity,
               formatted_progname, msg2str(msg)]
   end
 end
