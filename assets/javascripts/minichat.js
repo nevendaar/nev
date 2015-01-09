51298ab65803d25423726fd0b3cd4667899fb65d
Ivan <vansan@nevendaar.com>
2015-01-09 16:00:43 +0300

    Quickfix for mc profile links.
    > В чате при нажатии на имя теперь вместо ответа вылезает ссыль на профиль.
    
    http://nevendaar.com/forum/7-5-93792-16-1420389322

--- assets/javascripts/minichat.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    // Ссылки для копирования информации
-    $('.mc_username').click(function () {
-        //              window.parent ???
-        $('#mchatMsgF', parent.window.document).html(input.html() + '[i]' + $(this).html() + '[/i], ').focus();
-        return false;
-    });
-});
