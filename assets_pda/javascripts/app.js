4988f446dc5f688aaee4347e8e2277ead8ba437e
Ivan <vansan@nevendaar.com>
2015-01-25 21:18:11 +0300

    Use bootstrap 3 for PDA templates.

--- assets_pda/javascripts/app.js
+++ assets_pda/javascripts/app.js
@@ -6,4 +6,17 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// require bootstrap/affix
+// require bootstrap/alert
+// require bootstrap/button
+// require bootstrap/carousel
+//= require bootstrap/collapse
+// require bootstrap/dropdown
+// require bootstrap/tab
+// require bootstrap/transition
+// require bootstrap/scrollspy
+// require bootstrap/modal
+// require bootstrap/tooltip
+// require bootstrap/popover
+//
 //= require_tree .
